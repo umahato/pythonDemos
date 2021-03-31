ad640db3e26ac0cf76249dfadde82a0d1ff02a88
umahato <talk2umahato@gmail.com>
2021-03-31 13:09:29 +0530

    added few example in basic datatypes

---
+++ firstPython/hello.py
@@ -0,0 +1,23 @@
+Y = 20
+X = 10
+print (type(X))
+
+num = 10>9
+print(type(num))
+
+name = "Umesh"
+print(len(name))
+
+print(name[2])
+print(name[2:6])
+print(name.upper())
+print(name.lower())
+
+
+mylist = [10,20,30,"Umesh","shilu"]
+print(mylist)
+mylist.append(10)
+print(mylist)
+mylist.insert(5,100) # insert a value in specific position in a list
+
+print(mylist)

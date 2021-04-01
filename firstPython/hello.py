9c1bf0f040ef27aa23e495ae0dbd8426aa136e04
umahato <talk2umahato@gmail.com>
2021-04-01 13:40:01 +0530

    commit as on 1 apr

--- firstPython/hello.py
+++ firstPython/hello.py
@@ -1,3 +1,4 @@
+'''
 Y = 20
 X = 10
 print (type(X))
@@ -21,3 +22,60 @@ print(mylist)
 mylist.insert(5,100) # insert a value in specific position in a list
 
 print(mylist)
+mylist.reverse()
+print(mylist)
+
+
+
+### Dictionary mutable
+courses = {
+    1:'Python',
+    2:'Java',
+    3:'Data Science'
+}
+
+print(courses)
+
+print(courses[2])
+courses[2] = 'aws'
+print(courses)
+
+
+# Tuple -- immutable
+animals = (10,20,30,'tiger','lion','giraffe','tiger')
+print(animals[2])
+print(animals.count('tiger'))
+
+
+
+# set -- immutable
+
+myset = {10,20,30,40,50,40,'Umesh','Bangalore'}
+print(myset)
+
+
+
+
+#range
+
+print(range(10))
+print(list(range(11)))
+
+
+
+
+a = [1,2,3,4,5]
+b = {4,5,6,7,8}
+c = [a,b]
+print(c)
+
+
+
+# Type conversion.
+
+x = 10
+name = "umesh"
+
+print(str(x) + name )
+
+'''
